7e390a7a65fc0cde875984fc8a3f123d7f3c5560
Michael Vogt <michael.vogt@ubuntu.com>
2017-12-04 14:54:58 +0100

    snappy-repack: create vendor/no-vendor trees

--- snappy-repack.sh
+++ snappy-repack.sh
@@ -21,5 +21,15 @@ tar xf "$tmpdir/$(basename "$1")"
 
 new=snapd-"$2"
 mv snappy.upstream snapd-"$2"
+
+# full
 fakeroot tar cJf snapd_"$2".vendor.tar.xz snapd-"$2"
 cp -a snapd_"$2".vendor.tar.xz "$cur"
+
+# and one without vendor tree
+fakeroot tar -c --exclude 'snapd-*/vendor/*' -J -f snapd_"$2".no-vendor.tar.xz snapd-"$2"
+cp -a snapd_"$2".no-vendor.tar.xz "$cur"
+
+# and one with only vendor tree
+fakeroot tar -c -J -f snapd_"$2".only-vendor.tar.xz snapd-"$2"/vendor/
+cp -a snapd_"$2".only-vendor.tar.xz "$cur"
