e578e0dc3cf20ffd3c0dc4f16ce8ca8b59b59ae1
Michael Vogt <michael.vogt@ubuntu.com>
2017-12-12 16:10:22 +0100

    generate debian changelog diff as well

--- snaps-pkgs-diff.sh
+++ snaps-pkgs-diff.sh
@@ -5,10 +5,10 @@ set -e
 echo "Packages diff of snap $1 and $2"
 
 tmp=$(mktemp -d)
-tmp1=${tmp}/1
-tmp2=${tmp}/2
+tmp1=${tmp}/a
+tmp2=${tmp}/b
 
-trap "rm -rf $tmp" EXIT 
+#trap "rm -rf $tmp" EXIT 
 
 unsquashfs -n -d "$tmp1" "$1" /usr/share/snappy/dpkg.list >/dev/null
 unsquashfs -n -d "$tmp2" "$2" /usr/share/snappy/dpkg.list >/dev/null
@@ -18,5 +18,11 @@ diff -u0 \
      | egrep -v '^(---|\+\+\+)' | sed 's/^@@.*//g'
 
 
+echo "Package changelogs diff"
 
-
+rm -rf $tmp/{a,b}
+unsquashfs -n -d "$tmp1" "$1" /usr/share/doc/*/changelog.Debian.gz >/dev/null
+unsquashfs -n -d "$tmp2" "$2" /usr/share/doc/*/changelog.Debian.gz >/dev/null
+find $tmp -type l -name "changelog.Debian.gz" | xargs rm -f
+find $tmp -type f -name "changelog.Debian.gz" | xargs gzip -d
+(cd $tmp ; diff -u0 -N -r "a" "b" | grep -v "^-")
